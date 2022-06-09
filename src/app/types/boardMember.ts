539d9265bca30a1e9916c1031c1305e8581f3a32
Jeremy Wolfe <jeremy.wolfe@gm.com>
2022-06-09 10:28:45 -0400

    Round 1 of discussion finished

---
+++ src/app/types/boardMember.ts
@@ -0,0 +1,10 @@
+export class Doc {
+    id?: Number;
+    friendlyName: String;
+    name: String;
+    description: String;
+    item: String;
+    category: String;
+    editableColumns?: String;
+}
+
