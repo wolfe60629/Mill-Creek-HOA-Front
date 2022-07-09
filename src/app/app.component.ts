324ad8d06e8f6c547d29388f964dfb969fa887e5
Jeremy Wolfe <jeremy.wolfe@gm.com>
2022-07-09 00:19:23 -0400

    Added google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+import {filter} from 'rxjs';
+
+declare var gtag;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mill Creek Community Association';
+    constructor(router: Router) {
+     const navEndEvents = router.events.pipe(
+          filter(event => event instanceof NavigationEnd),
+      );
 
+     navEndEvents.subscribe((event: NavigationEnd) => {
+         gtag('config', 'G-GHEE5RV5RQ' , {
+             'page_path': event.urlAfterRedirects
+         });
+     });
+    }
 }
