37a7c4a2d725c72ce996ca8c70cbf3f62da02540
Jeremy Wolfe <jeremy.wolfe@gm.com>
2023-05-19 00:36:10 -0400

    Modern Look for about us page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,4 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
   ngOnInit(): void {
   }
+
+  jumpUp( index) {
+    const card = document.getElementsByClassName('card')[index] as HTMLElement;
+    card.style.transform = 'translateY(-5px)';
+    card.style.height = '450px';
+  }
+
+  resetPosition(index) {
+    const card = document.getElementsByClassName('card')[index] as HTMLElement;
+    card.style.transform = 'none';
+    card.style.height = '155px';
+  }
 }
