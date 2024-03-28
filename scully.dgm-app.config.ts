a455e5158869ba57bf9d9713d0573f83b2267b83
Jeremy Wolfe <jeremy.wolfe@gm.com>
2024-03-27 23:19:58 -0400

    try copy file plugin config

--- scully.dgm-app.config.ts
+++ scully.dgm-app.config.ts
@@ -1,7 +1,9 @@
-import { ScullyConfig } from '@scullyio/scully';
+import {registerPlugin, ScullyConfig} from '@scullyio/scully';
+
 
 /** this loads the default render plugin, remove when switching to something else. */
 import '@scullyio/scully-plugin-puppeteer'
+import * as fs from "fs-extra";
 
 export const config: ScullyConfig = {
   projectRoot: "./src",
@@ -12,3 +14,27 @@ export const config: ScullyConfig = {
   },
   extraRoutes: [ '/documents' ]
 };
+
+
+async function copyScriptFile() {
+  try {
+    // Source and destination paths for the script file
+    const sourcePath = './dist/static/scripts.4db5fb272383bd6a.js';
+    const destinationPath = './dist/static/documents/scripts.4db5fb272383bd6a.js';
+
+    // Copy the script file from the source path to the destination path
+    await fs.copy(sourcePath, destinationPath);
+
+    console.log('Script file copied successfully.');
+  } catch (error) {
+    console.error('Error copying script file:', error);
+  }
+}
+
+async function customAllDonePlugin() {
+  // Call the function to copy the script file
+  await copyScriptFile();
+}
+
+const validator = async () => [];
+registerPlugin('allDone', 'customAllDonePlugin', customAllDonePlugin, validator);
