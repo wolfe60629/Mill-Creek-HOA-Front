e5bbc4a22fda907fd81c6c9c17cd2ddb0a727a75
wolfe60629 <jeremy.wolfe@gm.com>
2024-04-05 22:04:33 -0400

    Added more modern amenities page

--- src/app/amenities/amenities.component.ts
+++ src/app/amenities/amenities.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CommunityEvent} from '../types/communityEvent';
 import {EventService} from '../services/event.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-amenities',
@@ -8,10 +9,40 @@ import {EventService} from '../services/event.service';
   styleUrls: ['./amenities.component.css']
 })
 export class AmenitiesComponent implements OnInit {
+  visibleSidebar1: boolean;
+  visibleSidebar2: boolean;
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   ngOnInit(): void {
+    // if ?poolRules is in the URL, open the pool rules sidebar
+    if (window.location.href.includes('?poolRules=true')) {
+      this.visibleSidebar1 = true;
+    }
+
+    // if ?courtRules is in the URL, open the court rules sidebar
+    if (window.location.href.includes('?courtRules=true')) {
+      this.visibleSidebar2 = true;
+    }
   }
 
+  openCourtRules() {
+    // Add ?courtRules=true to the end of the URL
+    this.location.replaceState(this.location.path() + '?courtRules=true');
+    this.visibleSidebar2 = true;
+  }
+
+  openPoolRules() {
+    // Add ?courtRules=true to the end of the URL
+    this.location.replaceState(this.location.path() + '?poolRules=true');
+    this.visibleSidebar1 = true;
+  }
+
+  closeRules() {
+    this.visibleSidebar1 = false;
+    this.visibleSidebar2 = false;
+
+    // remove ? from href
+    this.location.replaceState(this.location.path().split("?")[0]);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -48,6 +48,7 @@ import {RadioButtonModule} from 'primeng/radiobutton';
 import {SpinnerModule} from 'primeng/spinner';
 import {ProgressSpinnerModule} from 'primeng/progressspinner';
 import {CardModule} from 'primeng/card';
+import {SidebarModule} from "primeng/sidebar";
 
 const appRoutes: Routes = [
   {
@@ -130,29 +131,30 @@ const appRoutes: Routes = [
     CalendarComponent,
     NewsletterUploadComponent
   ],
-    imports: [
-        RouterModule.forRoot(appRoutes, {enableTracing: false, relativeLinkResolution: 'legacy'}),
-        BrowserModule,
-        MaterializeModule,
-        HttpClientModule,
-        FormsModule,
-        AccordionModule,
-        BrowserAnimationsModule,
-        ButtonModule,
-        ToastModule,
-        DropdownModule,
-        MultiSelectModule,
-        AutoCompleteModule,
-        NgxExtendedPdfViewerModule,
-        TableModule,
-        ListboxModule,
-        ConfirmDialogModule,
-        FullCalendarModule,
-        RadioButtonModule,
-        SpinnerModule,
-        ProgressSpinnerModule,
-        CardModule
-    ],
+  imports: [
+    RouterModule.forRoot(appRoutes, {enableTracing: false, relativeLinkResolution: 'legacy'}),
+    BrowserModule,
+    MaterializeModule,
+    HttpClientModule,
+    FormsModule,
+    AccordionModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    ToastModule,
+    DropdownModule,
+    MultiSelectModule,
+    AutoCompleteModule,
+    NgxExtendedPdfViewerModule,
+    TableModule,
+    ListboxModule,
+    ConfirmDialogModule,
+    FullCalendarModule,
+    RadioButtonModule,
+    SpinnerModule,
+    ProgressSpinnerModule,
+    CardModule,
+    SidebarModule,
+  ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
